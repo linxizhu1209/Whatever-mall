4bb9075ee05c2448ec84e704d84a4ca76ddd0cf5
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-07-15 19:57:29 +0900

    fix: ecs 배포를 위해 product서비스 수정
    - ecs 환경에 맞는 url 변경
    - 환경변수 추가

---
+++ product-service/set-ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# IP 주소 가져오기
+IP_ADDRESS=$(hostname -i)
+
+# 환경 변수 설정
+export EUREKA_INSTANCE_IP=$IP_ADDRESS
+
+# Java 애플리케이션 실행
+exec java -Djava.security.egd=file:/dev/./urandom -jar /app/ProductService.jar
